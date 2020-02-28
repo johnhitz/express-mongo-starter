d5f2c2ec80da398fa8be74734573e105d27802e9
John Hitz <johnh@whodapet.com>
2020-02-28 15:48:11 -0600

    added show and edit routes stubs

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -9,9 +9,21 @@ Authentication Middleware
 /*************************************************
 Presentation Routes
 *************************************************/
+// Index
 posts.get('/', (req, res) => {
   res.send('Hello Index')
 })
+
+// Show
+posts.get('/:id', (req, res) => {
+  res.send('Hello Show')
+})
+
+// Edit
+posts.get('/:id/edit', (req, res) => {
+  res.send('Hello edit')
+})
+
 
 /*************************************************
 Functional Routes

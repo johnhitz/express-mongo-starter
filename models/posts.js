b17861d391a551b51980c7bd59495b60fe3671ef
John Hitz <johnh@whodapet.com>
2020-03-05 01:10:46 -0600

    added timestamps and messed with styling to no avail

--- models/posts.js
+++ models/posts.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose')
 
 const postSchema = new mongoose.Schema({
-  title: { type: String, required: true, unique: true },
-  body: { type: String },
-  tags: [String],
-  image: { type: String }
-})
+    title: { type: String, required: true, unique: true },
+    body: { type: String },
+    tags: [String],
+    image: { type: String }
+  },
+  {
+    timestamps: true
+  }
+)
 
 const Post = mongoose.model('Post', postSchema)
 
